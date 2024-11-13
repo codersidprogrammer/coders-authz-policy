a6eec9e5ae0d37107d2377d3737111f60dd12331
codersidprogrammer <dimaseditiya.energi@gmail.com>
2024-11-14 00:13:40 +0700

    good job

--- src/main/java/com/github/codersidprogrammer/personal_number/PersonalNumberPolicyRepresentation.java
+++
@@ -1,9 +0,0 @@
-package com.github.codersidprogrammer.personal_number;
-
-import org.keycloak.representations.idm.authorization.PolicyRepresentation;
-
-public class PersonalNumberPolicyRepresentation extends PolicyRepresentation {
-
-    public PersonalNumberPolicyRepresentation () {}
-
-}
